26c27d35d60bbc1ca7e411a706e8bbfd83c1c326
danielogen <danielogen@gmail.com>
2023-10-24 18:21:47 -0700

    hotfix: test linting error

--- api/utils/setup_test.py
+++ api/utils/setup_test.py
@@ -1,9 +1,11 @@
 from rest_framework.test import APITestCase
 
+
 class TestSetUp(APITestCase):
     def setUp(self) -> None:
         print("Test started")
         return super().setUp()
+    
     
     def tearDown(self) -> None:
         print("Test finished")
