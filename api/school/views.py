dfad6c96cbd5253913fc9e72828138f52b205d0e
danielogen <danielogen@gmail.com>
2023-10-29 23:55:31 -0700

    chore: correct response status

--- api/school/views.py
+++ api/school/views.py
@@ -22,7 +22,7 @@ class SchoolAPIView(views.APIView):
                 "message": "School created successfully",
                 "data": serializer.data
             }
-            return Response(data=response, status=status.HTTP_201_OK)
+            return Response(data=response, status=status.HTTP_201_CREATED)
         
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
