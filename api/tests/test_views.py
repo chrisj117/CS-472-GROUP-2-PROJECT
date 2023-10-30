05eb2d49455fd7b830702cf89a718329e1ba641b
danielogen <danielogen@gmail.com>
2023-10-30 00:29:39 -0700

    feat(test): test update and delete endpoints

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -17,4 +17,67 @@ class TestSchoolAIPView(APITestCase):
 
         response = self.client.post(reverse('schools'), test_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_should_list_all_schools(self):
+
+        response = self.client.get(reverse('schools'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(len(response.data),0)
+        self.assertIsInstance(response.data['data'], list)
     
+    def test_should_return_one_school(self):
+        test_data = {
+            "short_name": "TEST",
+            "long_name": "The Test University",
+            "website": "https://test.university.edu",
+            "city": "Test City",
+            "state": "Test State",
+            "country": "Test Country"
+        }
+
+        response = self.client.post(reverse('schools'), test_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.get(reverse('school', kwargs={'short_name':"TEST"}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['data']['short_name'],'TEST')
+
+    def test_should_update_school(self):
+        test_data = {
+            "short_name": "TEST",
+            "long_name": "The Test University",
+            "website": "https://test.university.edu",
+            "city": "Test City",
+            "state": "Test State",
+            "country": "Test Country"
+        }
+
+        response = self.client.post(reverse('schools'), test_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        update_data = {
+            "short_name": "TEST",
+            "long_name": "The Test University",
+            "website": "https://update.university.edu",
+            "city": "Updated",
+        }
+        response = self.client.put(reverse('school', kwargs={'short_name':"TEST"}), update_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['data']['city'], 'Updated')
+        self.assertEqual(response.data['data']['website'], 'https://update.university.edu')
+    
+    def test_should_delete_school(self):
+        test_data = {
+            "short_name": "TEST",
+            "long_name": "The Test University",
+            "website": "https://test.university.edu",
+            "city": "Test City",
+            "state": "Test State",
+            "country": "Test Country"
+        }
+
+        response = self.client.post(reverse('schools'), test_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.delete(reverse('school', kwargs={'short_name':"TEST"}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data['data']), 0)
