178be8cb75ec89f9ed24c6f1842c2182e6f31aca
danielogen <danielogen@gmail.com>
2023-10-29 22:32:50 -0700

    feat: add url routes

--- api/backend/urls.py
+++ api/backend/urls.py
@@ -1,22 +1,25 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from rest_framework import routers
+from school import views as school_views
+from django.conf.urls import handler400, handler403, handler404, handler500
+from utils.views import error_404, error_500
 
-urlpatterns = [
+
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
+base_url = 'api/v1'
+
+# handler404 = error_404
+# handler500 = error_500
+handler404
+handler500
+
+urlpatterns += [
     path('admin/', admin.site.urls),
+    path(f'{base_url}/school/', school_views.SchoolAPIView.as_view()),
+    path(f'{base_url}/school/<str:short_name>/', school_views.SchoolAPIView.as_view()),
 ]
