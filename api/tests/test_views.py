2403a5d2aa6676eab517e9dccd3ed70d25e4be5b
danielogen <danielogen@gmail.com>
2023-10-29 23:57:09 -0700

    feat(test): creating a school

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,1 +1,19 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from school.views import SchoolAPIView
+
+
+class TestSchoolAIPView(APITestCase):
+    def test_should_create_school(self):
+        test_data = {
+            "short_name": "TEST",
+            "long_name": "The Test University",
+            "website": "https://test.university.edu",
+            "city": "Test City",
+            "state": "Test State",
+            "country": "Test Country"
+        }
+
+        response = self.client.post(reverse('schools'), test_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
