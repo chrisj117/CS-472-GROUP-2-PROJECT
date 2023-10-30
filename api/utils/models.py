74ee6410408153a12f2253e0191b80471d48ff3a
danielogen <danielogen@gmail.com>
2023-10-29 22:35:14 -0700

    feat: add util model for general purpose fields

---
+++ api/utils/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import uuid
+
+
+class TrackingModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+        ordering = ('-created_at')
