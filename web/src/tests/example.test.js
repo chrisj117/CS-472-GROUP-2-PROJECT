87e1ca2cf907a5f3f37620913af365362e71a591
azboss2021 <alexanderzyn1@gmail.com>
2023-11-01 18:38:41 -0700

    Added coverage reports when running app

--- web/src/tests/example.test.js
+++ web/src/tests/example.test.js
@@ -3,6 +3,6 @@ import { describe, expect, it } from 'vitest';
 
 describe('#example', () => {
   it('returns 5 with 3 and 2', () => {
-    expect(example(5)).toBe(5);
+    expect(example(3, 2)).toBe(5);
   });
 });
