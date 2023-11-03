50096f7553fa9fc09146bcc1bd951f3fb7e3c373
chrisj117 <chriswj2001@gmail.com>
2023-11-03 02:33:41 -0700

    return home button created

--- web/src/pages/About.jsx
+++ web/src/pages/About.jsx
@@ -1,6 +1,19 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div className="grid h-screen place-items-center">
+      {/* Top of page: Navbar (see components/Navbar.jsx) */}
+      {/* Middle of page: "About" content */}
+      
+      
+
+      {/* Bottom of page: Return home button*/}
+      <div className="py-6">
+        <button className="rounded-lg bg-blue-600 px-5 py-3 text-md font-medium text-white transition hover:bg-blue-700 focus:outline-none focus:ring"
+                type="button">
+                Return to home
+        </button>
+      </div>
+    </div>
   )
 }
 export default About
