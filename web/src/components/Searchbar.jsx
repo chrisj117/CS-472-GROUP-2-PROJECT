1af9b2c6ab318054ea1df639ca72b376a488303e
azboss2021 <alexanderzyn1@gmail.com>
2023-11-03 00:12:18 -0700

    Added temporary search bar to homepage for testing. Searchbar component WIP

--- web/src/components/Searchbar.jsx
+++ web/src/components/Searchbar.jsx
@@ -1,6 +1,30 @@
-const Searchbar = () => {
+import AsyncSelect from 'react-select/async';
+import { useEffect, useState } from 'react';
+
+const Searchbar = ({ schools, courses }) => {
+  const loadOptions = (searchValue, callback) => {
+    setTimeout(() => {
+      const filteredOptions = options.filter(
+        (option) =>
+          option.short_name.toLowerCase().includes(searchValue.toLowerCase()) ||
+          option.long_name.toLowerCase(searchValue.toLowerCase())
+      );
+
+      console.log('loadOptions', searchValue, filteredOptions);
+      callback(filteredOptions);
+    }, 2000);
+  };
+
+  const handleChange = (selectedOption) => {
+    console.log('handleChange', selectedOption);
+  };
+
   return (
-    <div>Searchbar</div>
-  )
-}
-export default Searchbar
+    <AsyncSelect
+      loadOptions={loadOptions}
+      defaultOptions
+      onChange={handleChange}
+    />
+  );
+};
+export default Searchbar;
