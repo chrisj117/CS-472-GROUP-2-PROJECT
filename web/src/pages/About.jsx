013870d0c86a21005a92a9e3df7206a8b4d5cb08
chrisj117 <chriswj2001@gmail.com>
2023-11-03 02:51:02 -0700

    added example text sections

--- web/src/pages/About.jsx
+++ web/src/pages/About.jsx
@@ -1,11 +1,21 @@
 const About = () => {
   return (
-    <div className="grid h-screen place-items-center">
+    <div className="flex flex-col items-center py-6 h-screen w-screen">
       {/* Top of page: Navbar (see components/Navbar.jsx) */}
       {/* Middle of page: "About" content */}
+      <h1 className="w-6/12 text-xl font-extrabold mb-4">Example header</h1>
+      <p className="w-6/12 text-m mb-8">
+        Example text - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h1 className="w-6/12 text-xl font-extrabold mb-4">Example header</h1>
+      <p className="w-6/12 text-m mb-8">
+        Example text - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h1 className="w-6/12 text-xl font-extrabold mb-4">Example header</h1>
+      <p className="w-6/12 text-m mb-8">
+        Example text - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
       
-      
-
       {/* Bottom of page: Return home button*/}
       <div className="py-6">
         <button className="rounded-lg bg-blue-600 px-5 py-3 text-md font-medium text-white transition hover:bg-blue-700 focus:outline-none focus:ring"
