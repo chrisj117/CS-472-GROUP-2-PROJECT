5e122995f74003d2ba90cb8e226fe986a649c730
angadb27 <bhatta5@unlv.nevada.edu>
2023-11-03 21:22:42 -0700

    updated term/grade/delivery to ChoiceField

--- api/review/admin.py
+++ api/review/admin.py
@@ -5,20 +5,20 @@ from .models import Review
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 
-        'school', 
-        'review_text', 
-        'term', 
-        'grade_received', 
-        'delivery_method', 
-        'helpful_count', 
-        'created_at', 
+        'id',
+        'school',
+        'review_text',
+        'term',
+        'grade_received',
+        'delivery_method',
+        'helpful_count',
+        'created_at',
         'updated_at'
     )
 
     list_filter = (
-        'school', 
-        'term', 
-        'grade_received', 
+        'school',
+        'term',
+        'grade_received',
         'delivery_method'
     )
--- api/review/serializers.py
+++ api/review/serializers.py
@@ -1,17 +1,18 @@
 from review.models import Review
 from rest_framework import serializers
-from rest_framework.fields import CharField, IntegerField
+from rest_framework.fields import CharField, IntegerField, ChoiceField
 from school.models import School
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # referencing the school's ID during review creation or editing. 
+    # referencing the school's ID during review creation or editing.
     school = serializers.PrimaryKeyRelatedField(queryset=School.objects.all())
-    
+
     review_text = CharField(required=True)
-    term = CharField(required=True, choices=Review.TERM_CHOICES)
-    grade_received = CharField(required=True, choices=Review.GRADE_CHOICES)
-    delivery_method = CharField(required=True, choices=Review.DELIVERY_CHOICES)
+    term = ChoiceField(required=True, choices=Review.TERM_CHOICES)
+    grade_received = ChoiceField(required=True, choices=Review.GRADE_CHOICES)
+    delivery_method = ChoiceField(
+        required=True, choices=Review.DELIVERY_CHOICES)
     helpful_count = IntegerField(required=False)
 
     class Meta:
