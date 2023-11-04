427cd31b5bf7fb232500072613ac9067496ff969
Ruaaann <somenewnames@gmail.com>
2023-11-03 17:19:40 -0700

    test_models.py updated

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from school.models import School
+from requestschool.models import RequestSchool
 
 
 class TestModels(TestCase):
@@ -9,3 +10,9 @@ class TestModels(TestCase):
                                        long_name="The Test University")
         school.save()
         self.assertEqual(str(school), "TEST")
+
+    def test_should_create_school_request(self):
+        school_req = RequestSchool.objects.create(school_name="TEST_U", 
+                                                  website="https://www.test-university.edu/")
+        school_req.save()
+        self.assertEqual(str(school_req), "TEST_U")
