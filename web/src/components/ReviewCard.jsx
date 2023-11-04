863c046c5f784aea79e4a188bafface5088bc576
azboss2021 <alexanderzyn1@gmail.com>
2023-11-04 11:54:22 -0700

    Comment out review card to avoid errors

--- web/src/components/ReviewCard.jsx
+++ web/src/components/ReviewCard.jsx
@@ -1,18 +1,18 @@
-/* eslint-disable react/prop-types */
-const ReviewCard = ({
-  subject,
-  additionalComments,
-  recommended,
-  date,
-  grade,
-  delivery,
-  textbook,
-  likes,
-  dislikes,
-  professor,
-  term,
-  year,
-}) => {
-  return <div className="border-2 border-gray-200">ReviewCard</div>;
-};
-export default ReviewCard;
+// /* eslint-disable react/prop-types */
+// const ReviewCard = ({
+//   subject,
+//   additionalComments,
+//   recommended,
+//   date,
+//   grade,
+//   delivery,
+//   textbook,
+//   likes,
+//   dislikes,
+//   professor,
+//   term,
+//   year,
+// }) => {
+//   return <div className="border-2 border-gray-200">ReviewCard</div>;
+// };
+// export default ReviewCard;
