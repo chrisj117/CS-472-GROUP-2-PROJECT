2215ef46bc1ce8da1c9563683cc919607d6d9ccf
azboss2021 <alexanderzyn1@gmail.com>
2023-11-05 06:53:51 -0800

    Remove props from review card

--- web/src/components/ReviewCard.jsx
+++ web/src/components/ReviewCard.jsx
@@ -1,19 +1,6 @@
 /* eslint-disable react/prop-types */
 
-const ReviewCard = ({
-  subject,
-  additionalComments,
-  recommended,
-  date,
-  grade,
-  delivery,
-  textbook,
-  likes,
-  dislikes,
-  professor,
-  term,
-  year,
-}) => {
+const ReviewCard = () => {
   return <div className="border-2 border-gray-200">ReviewCard</div>;
 };
 export default ReviewCard;
