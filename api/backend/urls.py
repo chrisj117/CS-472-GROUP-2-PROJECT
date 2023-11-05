4ab85eff5ec3f3e36c8cbf09e9b3f7ff58eac88f
danielogen <danielogen@gmail.com>
2023-11-05 13:41:49 -0800

    feat: add doc code

--- api/backend/urls.py
+++ api/backend/urls.py
@@ -1,17 +1,30 @@
 from django.urls import path
 from django.contrib import admin
 from rest_framework import routers
+from rest_framework import permissions
 from school import views as school_views
 from django.conf.urls import handler400, handler403, handler404, handler500
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title='My Course Evaluation API',
+        default_version='1.0.0',
+        contact=openapi.Contact(email='support@mycourseevaluation.com'),
+        license=openapi.License(name='MIT License'),
+        description='Comphrensive Course Evaluation API documentation'
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls
 base_url = 'api/v1'
 
-# handler404 = error_404
-# handler500 = error_500
 handler404
 handler500
 
@@ -19,4 +32,8 @@ urlpatterns += [
     path('admin/', admin.site.urls),
     path(f'{base_url}/school/', school_views.SchoolAPIView.as_view(), name='schools'),
     path(f'{base_url}/school/<str:short_name>/', school_views.SchoolAPIView.as_view(), name='school'),
+    # path('/swagger/schema', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
