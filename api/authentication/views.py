0224aec54dbae7693defd1a3056ff176ce85394e
danielogen <danielogen@gmail.com>
2023-11-13 13:13:26 -0800

    ft(auth): authentication app code

---
+++ api/authentication/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+from rest_framework import generics,status, views
+from .serializers import RegisterSerializer, EmailVerificationSerializer, LoginSerializer
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+import jwt
+from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .renderers import UserRender
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+    renderer_classes = (UserRender, )
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+
+        user = User.objects.get(email=user_data['email'])
+
+        token = RefreshToken.for_user(user).access_token
+
+        current_site = get_current_site(request).domain
+        relative_link = reverse('email-verify')
+        absurl = f'http://{current_site}{relative_link}?token={str(token)}'
+        email_body =f"""<tr> 
+                            <td
+                                style='color: #344054; font-size: 16px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word;'>
+                                Welcome to MyCourseEvaluation 🎉
+                                    <br />
+                                    Thanks for signing up; we can't wait for you to get started! Click the button to verify your email:
+                                    <br /><br />
+                                    <a href={absurl} target='_blank'>
+                                        <div
+                                            style='width: 20%; height: 100%; padding-left: 32px; padding-right: 32px; padding-top: 16px; padding-bottom: 16px; background: #135DFF; border-radius: 1px; justify-content: center; align-items: center; gap: 10px; display: inline-flex'>
+                                            <div
+                                                style='text-align: center; color: white; font-size: 16px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word'>
+                                                Verify Email</div>
+                                        </div>
+                                    </a>
+                                    <br /><br />
+                                    Trouble logging in? Paste this URL into your browser:
+                                    </br>
+                                    <a href={absurl} target='_blank'>{absurl}</a>
+                                    <br /><br />
+                                    <div
+                                        style='width: 100%; opacity: 0.60; color: #344054; font-size: 16px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word'>
+                                        You can set a permanent password anytime within your MyCourseEvaluation personal settings<br />Didn't make this
+                                        request? You can safely ignore and delete this email</div>
+                                    <br />
+                                    <br />
+                                </td>
+                            </tr>"""
+        
+        # f'Hi {user.username} \n User the link below to verify your email. \n {absurl}'
+        data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'MyCourseEvaluation Account Activation'}
+
+        Util.send_email(data)
+        
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
+
+    token_param_config = openapi.Parameter('token',in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, verify=False)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+
+        except jwt.ExpiredSignatureError as e:
+            return Response({'erorr': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as e:
+            return Response({'erorr': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
