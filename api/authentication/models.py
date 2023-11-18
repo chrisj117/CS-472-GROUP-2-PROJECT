3e4f8eab41002a006571f8119617246c3fe8ec2e
danielogen <danielogen@gmail.com>
2023-11-18 15:52:47 -0800

    chore(auth): complete model testing

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -12,11 +12,11 @@ class UserManager(BaseUserManager):
 
     def create_user(self, username, email, password=None):
 
-        if username is None:
-            raise TypeError('Users should have a username')
+        if not username:
+            raise ValueError('Users should have a username')
         
-        if email is None:
-            raise TypeError('Users should have a email')
+        if not email:
+            raise ValueError('Users should have a email')
         
         user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
