035c1efc1563e356a0c74edf59ad6ed5a463ec33
danielogen <danielogen@gmail.com>
2023-11-18 23:06:20 -0800

    chore: add serializer method field

--- api/authentication/serializers.py
+++ api/authentication/serializers.py
@@ -36,7 +36,15 @@ class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=3)
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
     username = serializers.CharField(max_length=255, min_length=3, read_only=True)
-    tokens = serializers.CharField(max_length=68, min_length=6, read_only=True)
+    tokens = serializers.SerializerMethodField()
+
+    def get_tokens(self, obj):
+        user = User.objects.get(email=obj['email'])
+
+        return {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
 
     class Meta:
         model = User
