d728596519e77f6411d0f7a4242b4eaa05946248
azboss2021 <alexanderzyn1@gmail.com>
2023-11-21 14:17:44 -0800

    Updated logo when resolution smaller and gap of navbar elements

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -4,37 +4,40 @@ import { MdSchool } from "react-icons/md"
 
 const Navbar = () => {
   return (
-    <header className="bg-white text-black py-6 px-10 flex justify-between items-center border-b-2 border-gray-200">
+    <header className="bg-white text-black py-6 xl:px-10 px-6 flex justify-between items-center border-b-2 border-gray-200">
       {/* Logo */}
       <Link to="/" className="flex items-center">
         <div>
-          <span className="text-black text-4xl font-extrabold">MyCourse</span>
-          <span className="text-blue-600 text-4xl font-extrabold">
+          <span className="hidden lg:inline-block text-black text-4xl font-extrabold">
+            MyCourse
+          </span>
+          <span className="hidden lg:inline-block text-blue-600 text-4xl font-extrabold">
             Evaluation
           </span>
         </div>
-        <MdSchool fontSize={36} />
+        <MdSchool className="text-5xl xl:text-2xl" />
       </Link>
       {/* Top Right Corner */}
       <div className="flex gap-6 items-center">
-        <button className="text-black px-4 py-2 rounded-lg hover:bg-gray-900 hover:text-white hover:border-gray-900 border-2 border-gray-300">
+        <button className="text-black px-2 py-2 xl:px-4 xl:py-2 rounded-lg hover:bg-gray-900 hover:text-white hover:border-gray-900 xl:border-2 border-gray-300">
           <div className="flex gap-2 items-center">
-            <span className="">Light Mode</span> <BsSun fontSize={21} />
+            <span className="hidden xl:block">Light Mode</span>{" "}
+            <BsSun className="text-2xl" />
           </div>
         </button>
-        <Link to="/about" className="text-black hover:underline text-lg">
+        <Link to="/about" className="text-black hover:underline xl:text-lg">
           About
         </Link>
         <Link
           to="/request-school"
-          className="text-black hover:underline text-lg"
+          className="text-black hover:underline xl:text-lg"
         >
           Request School
         </Link>
-        <Link to="/login" className="text-black hover:underline text-lg">
+        <Link to="/login" className="text-black hover:underline xl:text-lg">
           Login
         </Link>
-        <Link to="/register" className="text-black hover:underline text-lg">
+        <Link to="/register" className="text-black hover:underline xl:text-lg">
           Register
         </Link>
       </div>
