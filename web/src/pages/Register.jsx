d6fdf3a956b0ee5bdc6c0d15d9fa29f31866732f
chrisj117 <chriswj2001@gmail.com>
2023-11-22 19:12:22 -0800

    final style changes (spacing, pw complexity)

--- web/src/pages/Register.jsx
+++ web/src/pages/Register.jsx
@@ -2,7 +2,7 @@ const Register = () => {
   return (
     <div className="max-w-screen-xl mx-auto flex flex-col items-center">
       <div className="flex flex-col gap-3 w-full">
-      <div className="mb-4">
+      <div className="mb-4 mt-4">
         {/* E-mail input */}
         <label className="flex flex-col gap-1 text-lg">
           E-mail
@@ -36,7 +36,7 @@ const Register = () => {
           Password
           <input
             type="text"
-            placeholder="Password"
+            placeholder="Password (must be 8 characters long and include at least 1 numeric character)"
             className="px-4 py-2 w-full border-2 rounded"
           />
         </label>
