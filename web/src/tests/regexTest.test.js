7dc5700d5b58eb8295988c0e26fbf39b7750250c
azboss2021 <alexanderzyn1@gmail.com>
2023-11-29 19:38:28 -0800

    Create regex tests and get 100% coverage. Remove example test file

---
+++ web/src/tests/regexTest.test.js
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom';
+import { UsernameRegexTest, 
+  EmailRegexTest, 
+  PasswordRegexTest, 
+  LinkRegexTest, 
+  MaxCharacterTest, 
+  NormalCharactersTest } from '../utilities/RegexTest';
+import { describe, expect, it } from 'vitest';
+
+describe('#UsernameRegexTest', () => {
+  it('Returns false with special characters', () => {
+    expect(UsernameRegexTest('Test.Name')).toBe(false);
+  });
+
+  it('Returns true with normal characters', () => {
+    expect(UsernameRegexTest('TestName')).toBe(true);
+  });
+});
+
+describe('#EmailRegexTest', () => {
+  it('Returns false with non email format', () => {
+    expect(EmailRegexTest('Mail.com')).toBe(false);
+  });
+
+  it('Returns true with email format', () => {
+    expect(EmailRegexTest('name@mail.com')).toBe(true);
+  });
+});
+
+describe('#PasswordRegexTest', () => {
+  it('Returns false with too few characters', () => {
+    expect(PasswordRegexTest('123')).toBe(false);
+  });
+
+  it('Returns true with normal password input', () => {
+    expect(PasswordRegexTest('12345678')).toBe(true);
+  });
+});
+
+describe('#LinkRegexTest', () => {
+  it('Returns false with too few characters', () => {
+    expect(LinkRegexTest('normal text')).toBe(false);
+  });
+
+  it('Returns true with normal password input', () => {
+    expect(LinkRegexTest('website.com')).toBe(true);
+  });
+});
+
+describe('#LinkRegexTest', () => {
+  it('Returns false with too many characters', () => {
+    expect(MaxCharacterTest('normal text exceed max length', 1, 8)).toBe(false);
+  });
+
+  it('Returns true with in-range characters count', () => {
+    expect(MaxCharacterTest('normal text exceed max length', 1, 32)).toBe(true);
+  });
+});
+
+describe('#NormalCharacterTest', () => {
+  it('Returns false with special characters', () => {
+    expect(NormalCharactersTest('Dr* Professor')).toBe(false);
+  });
+
+  it('Returns true with legal characters', () => {
+    expect(NormalCharactersTest('First Last')).toBe(true);
+  });
+});
--- web/src/utilities/RegexTest.js
+++ web/src/utilities/RegexTest.js
@@ -1,12 +1,23 @@
 export function EmailRegexTest(data) {
-  return data.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+  return RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).test(data);
 }
 
 export function UsernameRegexTest(data) {
-  return data.match(/^[a-zA-Z0-9]{4,24}$/);
+  return RegExp(/^[a-zA-Z0-9]{4,24}$/).test(data)
 }
 
 export function PasswordRegexTest(data) {
-  return data.match(/^.{6,32}$/);
+  return RegExp(/^.{6,32}$/).test(data);
 }
 
+export function LinkRegexTest(data) {
+  return RegExp(/^(?:(?!(?:https?|ftp):\/\/)[^\s]+)$/).test(data);
+}
+
+export function MaxCharacterTest(data, minLength, maxLength) {
+  return RegExp(`^.{${minLength},${maxLength}}$`).test(data)
+}
+
+export function NormalCharactersTest(data) {
+  return RegExp(/^[a-zA-Z\s]*$/).test(data)
+}
