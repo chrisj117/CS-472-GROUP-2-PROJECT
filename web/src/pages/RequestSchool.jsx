1355bf79ba6e15880f5653df743877174e09c79a
chrisj117 <chriswj2001@gmail.com>
2023-11-30 19:09:27 -0800

    x padding on inputs, vertical clip fix, font sizes

--- web/src/pages/RequestSchool.jsx
+++ web/src/pages/RequestSchool.jsx
@@ -3,19 +3,19 @@ import { BsFillEnvelopePaperFill } from "react-icons/bs"
 
 const RequestSchool = () => {
   return (
-    <div className="max-w-screen-xl mx-auto flex flex-col items-center h-[calc(100vh-94px)]">
-      <h2 className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold mb-12 mt-8">Request School</h2>
+    <div className="overflow-auto max-w-screen-xl mx-auto flex flex-col items-center h-[calc(100vh-94px)]">
+      <h2 className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold mb-12 mt-8">Request School</h2>
       <div className="w-full flex flex-col items-center gap-4">
         {/* Check if your school is already here */}
         <div className="mb-4 flex flex-col w-full">
-          <p className="sm:text-base md:text-base lg:text-xl xl:text-2xl mb-4 text-center">
+          <p className="text-base md:text-lg lg:text-xl xl:text-2xl mb-4 text-center">
             <span className="font-bold text-blue-600">Wait!</span> Check if your
             school is already here:
           </p>
           <Searchbar
             searchingSchools={true}
             searchPlaceholder="Ex: University of Nevada, Las Vegas / UNLV"
-            className="w-full mb-8 truncate"
+            className="w-full mb-8 truncate px-4 text-sm md:text-base lg:text-lg"
           />
         </div>
         {/* Request a School Form */}
@@ -23,11 +23,11 @@ const RequestSchool = () => {
 
         <div className="flex flex-col gap-3 w-full">
           <div className="mb-4">
-            <p className="sm:text-base md:text-lg lg:text-xl xl:text-2xl mb-4 text-center">
+            <p className="text-base md:text-lg lg:text-xl xl:text-2xl mb-4 text-center">
               If you can&apos;t find your institution above, submit your
               school&apos;s information below!
             </p>
-            <label className="flex flex-col gap-1 sm:text-sm md:text-base lg:text-lg">
+            <label className="flex flex-col gap-1 text-sm md:text-base lg:text-lg px-4">
               Enter School Name
               <input
                 type="text"
@@ -38,7 +38,7 @@ const RequestSchool = () => {
           </div>
 
           <div className="mb-4">
-            <label className="flex flex-col gap-1 sm:text-sm md:text-base lg:text-lg">
+            <label className="flex flex-col gap-1 text-sm md:text-base lg:text-lg px-4">
             Enter School Website
             <input
               type="text"
