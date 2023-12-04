09dba90947945902b60902a21784bd09b75f9b43
angadb27 <bhatta5@unlv.nevada.edu>
2023-12-03 17:00:40 -0800

    Modified Ryan's scrape.py to also scrape for professors

--- api/UNLV_course_data/scrape.py
+++ api/UNLV_course_data/scrape.py
@@ -1,35 +1,43 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
-row_data = []
+# Store course data with professors
+course_data = defaultdict(set)
 
 # Not a dynamic way to go through every page, but works for our case.
 for i in range(0, 64):
     url = "https://www.unlv.edu/online/courses?page={0}".format(i)
-
-    # Send a GET request to the URL
     response = requests.get(url)
-    print(response)
 
     soup = BeautifulSoup(response.text, "html.parser")
     table = soup.find("table", class_ = "table table-hover")
 
     for row in table.find_all('tr'):
-        col = row.find_all('td')
-        col = [tr.text.strip() for tr in col]
-        row_data.append(col)
-    
-    #time.sleep(1)
+        cols = row.find_all('td')
+        if cols:
+            subject = cols[0].text.strip()
+            catalog_number = cols[1].text.strip()
+            title = cols[2].text.strip()
+            professor = cols[3].text.strip()
 
-df = pd.DataFrame(row_data)
+            course_key = (subject, catalog_number, title)
 
-# Drop professor and type columns, drop empty rows
-df.drop(df.columns[[3,4]], axis=1, inplace=True)
-df.dropna(inplace=True)
+            course_data[course_key].add(professor)
 
-# Drop all duplicate courses
-df.drop_duplicates(keep='first', inplace=True)
+rows = []
+for (subject, catalog_number, title), professors in course_data.items():
+    row = {
+        'Subject': subject,
+        'Catalog Number': catalog_number,
+        'Title': title,
+        'Professors': ', '.join(professors)
+    }
+    rows.append(row)
+
+# Create DataFrame
+df = pd.DataFrame(rows)
 
 # Save to csv file with appropriate header
-df.to_csv('courses.csv', sep=',', index=False, header=['Subject', 'Catalog Number', 'Title'])
+df.to_csv('courses_with_professors.csv', sep=',', index=False)
