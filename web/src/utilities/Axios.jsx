4b98ae9a8359e8b24159979c5ca984c085cdb0fb
danielogen <danielogen@gmail.com>
2023-12-05 16:39:48 -0800

    fix: remove extract forward slash from base URL

--- web/src/utilities/Axios.jsx
+++ web/src/utilities/Axios.jsx
@@ -2,5 +2,5 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "https://cs-472-group-2-project-backend.vercel.app//api/v1",
+  baseURL: "https://cs-472-group-2-project-backend.vercel.app/api/v1",
 })
